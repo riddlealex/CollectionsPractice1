8292fb54340b39220e83f0a1bd57604e95feabb8
Alex <alxsandin@gmail.com>
2025-01-06 19:02:52 +0300

    done

---
+++ src/collections/work/task2/User.java
@@ -0,0 +1,27 @@
+package collections.work.task2;
+
+public class User {
+
+    private int id;
+    private String name;
+    private int age;
+
+    public User(int id, String name, int age) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
+}
